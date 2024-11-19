a3b857f15aa7ace90cf271d2b2a0a49a90a1a397
Ricardo <ricardo.alvear@georgebrown.ca>
2024-11-19 15:24:28 -0500

    Add files via upload

---
+++ labs/SQLite.sql
@@ -0,0 +1,7 @@
+-- Ricardo Alvear
+SELECT course_id, title, status, due_date
+FROM assignments
+WHERE status != 'Completed'	
+  AND course_id LIKE 'COMM%'
+  AND due_date < '2024-12-31'
+ORDER BY due_date;
